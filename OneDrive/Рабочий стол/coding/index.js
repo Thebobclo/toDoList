ad66017f063b03966b867077f66ea520284ba665
fyxyuc ucycy <shramov058@gmail.com>
2023-06-26 21:46:26 +0300

    htmlправка

--- OneDrive/Рабочий стол/coding/index.js
+++ OneDrive/Рабочий стол/coding/index.js
@@ -1,34 +1,57 @@
-let TODOS = [
-  {
-    id: Math.random(),
-    title: "",
-    description: "",
-    startdate: new Date(1995, 11, 17),
-    enddate: new Date(2000, 11, 17),
-    priority: "",
-    relatedTasks: [],
-  },
-];
-function addnewTask(newTodo) {
-  newTodo.id = Math.random(),
-  TODOS.push(newTodo);
-}
-function deleteTask(id) {
-  TODOS = TODOS.filter((todo) => id !== todo.id);
-}
-function updateTask(newOptions, id) {
-  TODOS = TODOS.map((todo) => {
-    if (todo.id === id) {
-      return { ...todo, ...newOptions };
-    }
-    return todo;
-  });
-}
+// let TODOS = [
+//   {
+//     id: Math.random(),
+//     title: "",
+//     description: "",
+//     startdate: new Date(1995, 11, 17),
+//     enddate: new Date(2000, 11, 17),
+//     priority: "",
+//     relatedTasks: [],
+//   },
+// ];
+// function addnewTask(newTodo) {
+//   (newTodo.id = Math.random()), TODOS.push(newTodo);
+// }
+// function deleteTask(id) {
+//   TODOS = TODOS.filter((todo) => id !== todo.id);
+// }
+// function updateTask(newOptions, id) {
+//   TODOS = TODOS.map((todo) => {
+//     if (todo.id === id) {
+//       return { ...todo, ...newOptions };
+//     }
+//     return todo;
+//   });
+// }
+
+// function seachName(TODOS) {
+//   TODOS.find((elm) => elm.title === "string");
+// }
+
+
+
+const addButton = document.querySelector(".inputField button");
+const textInputField = document.querySelector(".inputField input")
+const todoList  = document.querySelector(".todoList")
+
+addButton.addEventListener('click', (умуте)  => {
+
+  if(event.target.classList.contains('.addBtn'))
+  return 
+})
+
+  
+
+
+
+
+
+
+// document.querySelector(".addInput").value;
+// document.querySelector(".todoList").innerHTML == dataInput;
+
+ 
+
 
 //создать метод который переносит из TODOS[] в CompliteTODOS
 //метод котолрый ищет TODO по имени
-function seachName(TODOS){
-  TODOS.find(elm => elm.title === "string")
-
-}
-
