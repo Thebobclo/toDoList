bfabd822d1c81e48c64a9789ac74cf3e663e75bc
fyxyuc ucycy <shramov058@gmail.com>
2023-06-28 19:42:59 +0300

    jsфункциибазовые

--- OneDrive/Рабочий стол/coding/index.js
+++ OneDrive/Рабочий стол/coding/index.js
@@ -1,57 +1,50 @@
-// let TODOS = [
-//   {
-//     id: Math.random(),
-//     title: "",
-//     description: "",
-//     startdate: new Date(1995, 11, 17),
-//     enddate: new Date(2000, 11, 17),
-//     priority: "",
-//     relatedTasks: [],
-//   },
-// ];
-// function addnewTask(newTodo) {
-//   (newTodo.id = Math.random()), TODOS.push(newTodo);
-// }
-// function deleteTask(id) {
-//   TODOS = TODOS.filter((todo) => id !== todo.id);
-// }
-// function updateTask(newOptions, id) {
-//   TODOS = TODOS.map((todo) => {
-//     if (todo.id === id) {
-//       return { ...todo, ...newOptions };
-//     }
-//     return todo;
-//   });
-// }
+const todolist = [];
 
-// function seachName(TODOS) {
-//   TODOS.find((elm) => elm.title === "string");
-// }
+let value = "";
+const root = document.querySelector(".wrapper");
+const todoListContainer = document.querySelector(".todoListContainer");
+const addButton = document.querySelector("#btn2");
+const clearAllbtn = document.querySelector("#btnfooter");
+const footerContainer = document.querySelector(".footerCounter");
 
+addButton.addEventListener("click", (event) => {
+  todolist.push({
+    // id: Math.random(),
+    title: value,
+    status: "todo",
+  });
 
+  clear();
+  updateCounter();
+  render();
+});
+root.addEventListener("change", (event) => {
+  switch (true) {
+    case event.target.id === "textinput":
+      value = event.target.value;
+  }
+});
 
-const addButton = document.querySelector(".inputField button");
-const textInputField = document.querySelector(".inputField input")
-const todoList  = document.querySelector(".todoList")
+clearAllbtn.addEventListener("click", (event) => {
+  clear();
+  clearAll();
+});
 
-addButton.addEventListener('click', (умуте)  => {
+function render() {
+  todolist.forEach((todo) =>
+    todoListContainer.insertAdjacentHTML("beforeend", `<li>${todo.title}</li> `)
+  );
+}
 
-  if(event.target.classList.contains('.addBtn'))
-  return 
-})
+function clear() {
+  todoListContainer.innerHTML = "";
+  footerContainer.innerHTML = "";
+}
 
-  
+function clearAll() {
+  todolist.splice(0, todolist.length);
+}
 
-
-
-
-
-
-// document.querySelector(".addInput").value;
-// document.querySelector(".todoList").innerHTML == dataInput;
-
- 
-
-
-//создать метод который переносит из TODOS[] в CompliteTODOS
-//метод котолрый ищет TODO по имени
+function updateCounter() {
+  footerContainer.innerHTML = `<p>У вас ${todolist.length} задач </p>`;
+}
