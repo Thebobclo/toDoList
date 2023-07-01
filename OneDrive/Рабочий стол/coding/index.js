1982350ff72f1a82c9629a61142efc54590784e9
fyxyuc ucycy <shramov058@gmail.com>
2023-07-01 20:26:30 +0300

    test

--- OneDrive/Рабочий стол/coding/index.js
+++ OneDrive/Рабочий стол/coding/index.js
@@ -1,5 +1,4 @@
-const todolist = [];
-
+let todolist = [];
 let value = "";
 const root = document.querySelector(".wrapper");
 const todoListContainer = document.querySelector(".todoListContainer");
@@ -8,20 +7,31 @@ const clearAllbtn = document.querySelector("#btnfooter");
 const footerContainer = document.querySelector(".footerCounter");
 
 addButton.addEventListener("click", (event) => {
-  todolist.push({
-    // id: Math.random(),
-    title: value,
-    status: "todo",
-  });
-
-  clear();
-  updateCounter();
-  render();
+  switch (true) {
+    case event.target.className === "fas fa-plus":
+      todolist.push({
+        id: Math.random(),
+        title: value,
+        status: "todo",
+      });
+      clear();
+      updateCounter();
+      render();
+    case event.target.classlist.contains ("toggleStatus"):
+      objtoChange = todolist.find((todo) => todo.id === event.target.dataset.id);
+      console.log(event.target.dataset);
+      objtoChange.status = objtoChange.status === "todo" ? "done" : "todo";
+  }
 });
+
 root.addEventListener("change", (event) => {
   switch (true) {
     case event.target.id === "textinput":
       value = event.target.value;
+    // case event.target.classlist.contains("toggleStatus"):
+    //   objtochange = todolist.find((todo) => todo.id === event.target.dataset.id);
+    //   console.log(event.target.dataset)
+    //   objtochange.status = objtochange.status === "todo" ? "done" : "todo";
   }
 });
 
@@ -32,7 +42,15 @@ clearAllbtn.addEventListener("click", (event) => {
 
 function render() {
   todolist.forEach((todo) =>
-    todoListContainer.insertAdjacentHTML("beforeend", `<li>${todo.title}</li> `)
+    todoListContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div class="todolistLeft"<ul> <li>${todo.title}</li></ul> </div>
+    <div class="todolistRight" 
+    <button  data-id="${todo.id}" class="toggleStatus" type="button">
+    <i class="fa fa-check"></i></button>   
+   <button   data-id="${todo.id}" class="toggleStatus" type="button">
+    <i class="fa fa-list"></i></button>   </div>`
+    )
   );
 }
 
